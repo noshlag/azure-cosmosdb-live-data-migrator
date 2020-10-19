11371881e8c389fe5e62904eace0994a5cd3018f
Nathan Oshlag <n.oshlag@gmail.com>
2020-10-19 11:15:33 -0700

    Removing empty User documents from migration and also documents that are not User or Action documents.

--- MigrationExecutorApp/DocumentFeedObserver.cs
+++ MigrationExecutorApp/DocumentFeedObserver.cs
@@ -19,8 +19,10 @@
     using Microsoft.Azure.Documents.ChangeFeedProcessor.FeedProcessing;
     using Microsoft.Azure.Documents.Client;
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
     using ChangeFeedObserverCloseReason = Microsoft.Azure.Documents.ChangeFeedProcessor.FeedProcessing.ChangeFeedObserverCloseReason;
     using IChangeFeedObserver = Microsoft.Azure.Documents.ChangeFeedProcessor.FeedProcessing.IChangeFeedObserver;
+    using MsnUser = Msn.TagsDataModel.TagEntityLib.User;
 
     public class DocumentFeedObserver: IChangeFeedObserver
     {
@@ -68,6 +70,12 @@
             return Task.CompletedTask;
         }
 
+        public static JsonSerializerSettings jsonSettings = new JsonSerializerSettings()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
         public async Task ProcessChangesAsync(
             IChangeFeedObserverContext context, 
             IReadOnlyList<Document> docs, 
@@ -81,32 +89,166 @@
 
                 List<Document> transformedDocs = new List<Document>();
                 Document document = new Document();
-                foreach (var doc in docs)
+                var newDocs = docs.Where(doc =>
                 {
-                    document = (SourcePartitionKeys != null & TargetPartitionKey != null) ? MapPartitionKey(doc, isSyntheticKey, TargetPartitionKey, isNestedAttribute, SourcePartitionKeys) : document = doc;
-                    transformedDocs.AddRange(documentTransformer.TransformDocument(document).Result);
-                }
+                    string typeName = doc.GetPropertyValue<string>("_t");
+                    if (typeName == null) return false;  // If we have no typeName, then it definitely isn't a User or Action
+                    if (typeName.Equals("Action")) return true;  // All actions should be copied over
+                    if (typeName.Equals("User"))  //Only non-empty User actions should be copied over. 
+                    {
+                        try
+                        {
+                            //I'm not certain about this method of converting the Document into an MsnUser.  I might need to change this line
+                            MsnUser msnUser = JsonConvert.DeserializeObject<MsnUser>(JsonConvert.SerializeObject(doc, jsonSettings));
 
-                bulkImportResponse = await bulkExecutor.BulkImportAsync(
-                    documents: transformedDocs,
-                    enableUpsert: true,
-                    maxConcurrencyPerPartitionKeyRange: 1,
-                    disableAutomaticIdGeneration: true,
-                    maxInMemorySortingBatchSize: null,
-                    cancellationToken: new CancellationToken(),
-                    maxMiniBatchSizeBytes: 100 * 1024);
 
-                if (bulkImportResponse.FailedImports.Count > 0 && containerClient != null)
+                            if (!string.IsNullOrWhiteSpace(msnUser.Email))
+                                return true; //Never going to happen
+                            if (!string.IsNullOrWhiteSpace(msnUser.UserName))
+                                return true; //Never going to happen
+                            if (!string.IsNullOrWhiteSpace(msnUser.UserProfile))
+                                return true; //Never going to happen
+                            if (!string.IsNullOrWhiteSpace(msnUser.UserPictureUrl))
+                                return true; //Never going to happen
+                            if (!string.IsNullOrWhiteSpace(msnUser.UserLevel))
+                                return true; //Never going to happen
+
+                            if (msnUser.IsMigrationConsentGiven != null)
+                                return true;
+                            if (msnUser.NewsAppMigrated?.Count > 0)
+                                return true;
+                            if (!string.IsNullOrEmpty(msnUser.UserLayoutPreference))
+                                return true;
+                            if (!string.IsNullOrWhiteSpace(msnUser.WeatherDisplayUnit))
+                                return true;
+                            if (msnUser.HasActions?.Count > 0)
+                                return true;
+                            if (msnUser.AnyActions != null)
+                                return true;
+                            if (msnUser.PdpFacetsMigrated?.Count > 0)
+                                return true;
+
+                            /* Vishakha Said to remove because she owns them and knows they are not needed
+                            if (msnUser.FinancePdpMigrated?.Count > 0)              return true; //Never going to happen
+                            if (msnUser.SportsPdpMigrated?.Count > 0)               return true; //Never going to happen
+                            if (msnUser.LocationPdpMigrated?.Count > 0)             return true; //Never going to happen
+                            if (msnUser.NewsPdpMigrated)                            return true; //Never going to happen
+                            */
+
+                            if (!string.IsNullOrWhiteSpace(msnUser.EntityName))
+                                return true; //Never going to happen
+                            if (msnUser.PageViewsPropertyBag?.Count > 0)
+                                return true; //Never going to happen
+                            if (msnUser.PdpMarketMigrated == true)
+                                return true; //Never going to happen
+                            if (!string.IsNullOrWhiteSpace(msnUser.PrimeMarketPreference))
+                                return true; //Never going to happen
+
+                            if (msnUser.PrimeMarketConfig != null)
+                                return true;
+                            if (msnUser.PrimeStripeSelection != null)
+                                return true;
+
+                            if (msnUser.PdpStripeMigrated == true)
+                                return true; //Never going to happen
+                            if (msnUser.AnaheimSettings != null)
+                                return true; //Never going to happen
+
+                            if (msnUser.PdpWatchlistSetting != null)
+                                return true;
+
+                            if (!string.IsNullOrWhiteSpace(msnUser.Type))
+                                return true; //Never going to happen
+                            if (!string.IsNullOrWhiteSpace(msnUser.Url))
+                                return true; //Never going to happen
+                            if (!string.IsNullOrWhiteSpace(msnUser.SourceHref))
+                                return true; //Never going to happen
+                            if (!string.IsNullOrWhiteSpace(msnUser.Locale))
+                                return true; //Never going to happen
+                            if (!string.IsNullOrWhiteSpace(msnUser.AdaptiveCard))
+                                return true; //Never going to happen
+
+                            if (!string.IsNullOrWhiteSpace(msnUser.SegmentCardSettings))
+                                return true;
+                            if (msnUser.UserSettings?.Count > 0)
+                                return true;
+
+#if false // This was the old attempt at the code.  I'm keeping it around for now because it has some of my notes about which properties are ignorable.
+                            // Run a bunch of tests on the resulting msnAction document.
+                            //if (doc.GetPropertyValue<string>("email") != null)                      return true; //None
+                            //if (doc.GetPropertyValue<string>("userName") != null)                   return true; //None
+                            //if (doc.GetPropertyValue<string>("userProfile") != null)                return true; //None
+                            //if (doc.GetPropertyValue<string>("userPictureUrl") != null)             return true; //None
+                            //if (doc.GetPropertyValue<string>("userLevel") != null)                  return true; //None
+                            if (doc.GetPropertyValue<string>("isMigrationConsentGiven") != null)    return true; //Has Results
+                            if (doc.GetPropertyValue<object>("newsAppMigrated") != new object())    return true; //Has Results
+                            if (doc.GetPropertyValue<string>("userLayoutPreference") != null)       return true; //Has Results
+                            if (doc.GetPropertyValue<string>("weatherDisplayUnit") != null)         return true; //Has Results
+                            if (doc.GetPropertyValue<object>("hasActions") != null
+                                && doc.GetPropertyValue<object>("hasActions") != new object())      return true; //Has Results 
+                            if (doc.GetPropertyValue<bool?>("anyActions") != null)                  return true; //Has Results
+                            if (doc.GetPropertyValue<object>("pdpFacetsMigrated") != new object())  return true; //Has Results
+                            //if (doc.GetPropertyValue<object>("financePdpMigrated") != new object())     return true;
+                            //if (doc.GetPropertyValue<object>("sportsPdpMigrated") != new object())      return true;
+                            //if (doc.GetPropertyValue<object>("locationPdpMigrated") != new object())    return true;
+                            //if (doc.GetPropertyValue<bool>("newsPdpMigrated") != false)                 return true;
+                            //if (doc.GetPropertyValue<string>("name") != null)                       return true;
+                            //if (doc.GetPropertyValue<object>("pageViewsPropertyBag") != new object())   return true;
+                            //if (doc.GetPropertyValue<bool>("pdpMarketMigrated") != false)               return true; //Only False
+                            //if (doc.GetPropertyValue<string>("primeMarketPreference") != null)      return true;//None
+                            if (doc.GetPropertyValue<object>("primeMarketConfig") != null)          return true; //Has Results
+                            if (doc.GetPropertyValue<object>("primeStripeSelection") != null) return true; //Has Results
+                            //if (doc.GetPropertyValue<bool>("pdpStripeMigrated") != false)               return true; //No Results
+                            //if (doc.GetPropertyValue<string>("anaheimSettings") != null)            return true; //No Results
+                            if (doc.GetPropertyValue<object>("pdpWatchlistSetting") != null)        return true; //Has Results
+                            //if (doc.GetPropertyValue<string>("type") != null)                       return true;// One Useless Result
+                            //if (doc.GetPropertyValue<string>("url") != null)                        return true;//No Results
+                            //if (doc.GetPropertyValue<string>("sourceHref") != null)                 return true;//No Results
+                            //if (doc.GetPropertyValue<string>("locale") != null)                     return true;//No Results
+                            //if (doc.GetPropertyValue<string>("adaptiveCard") != null)               return true;//No Results
+#endif
+
+                        }
+                        catch (Exception ex)
+                        {
+                            //We may want to add some logging here.
+                            return false;
+                        }
+                    }
+                    return false;
+                });
+
+                if (newDocs.Any())
                 {
-                    WriteFailedDocsToBlob("FailedImportDocs", containerClient, bulkImportResponse);
-                }
+                    // Apply any necessary Partition Key mappings and transform the documents.  For the User and Action migration, this will be a NOOP, 
+                    // but it's still worth keeping the code in so that we have access to the functionality if it turns out we need it later.
+                    foreach (var doc in newDocs)
+                    {
+                        document = (SourcePartitionKeys != null & TargetPartitionKey != null) ? MapPartitionKey(doc, isSyntheticKey, TargetPartitionKey, isNestedAttribute, SourcePartitionKeys) : document = doc;
+                        transformedDocs.AddRange(documentTransformer.TransformDocument(document).Result);
+                    }
 
-                if (bulkImportResponse.BadInputDocuments.Count > 0 && containerClient != null)
-                {
-                    WriteFailedDocsToBlob("BadInputDocs", containerClient, bulkImportResponse);
-                }
+                    bulkImportResponse = await bulkExecutor.BulkImportAsync(
+                        documents: transformedDocs.ToList(),
+                        enableUpsert: false,
+                        maxConcurrencyPerPartitionKeyRange: 1,
+                        disableAutomaticIdGeneration: true,
+                        maxInMemorySortingBatchSize: null,
+                        cancellationToken: new CancellationToken());
 
-                LogMetrics(context, bulkImportResponse);
+                    if (bulkImportResponse.FailedImports.Count > 0 && containerClient != null)
+                    {
+                        WriteFailedDocsToBlob("FailedImportDocs", containerClient, bulkImportResponse);
+                    }
+
+                    if (bulkImportResponse.BadInputDocuments.Count > 0 && containerClient != null)
+                    {
+                        WriteFailedDocsToBlob("BadInputDocs", containerClient, bulkImportResponse);
+                    }
+
+
+                    LogMetrics(context, bulkImportResponse);
+                }
             }
             catch (Exception e)
             {
